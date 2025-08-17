94d1ec770136cc9cae8a04ed88617938f95c3faf
Daniil Bastrich <bastrich+github@pm.me>
2025-08-17 02:01:32 +0200

    Continue experimenting

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,9 +1,8 @@
-use std::io::Read;
+use std::hash::Hash;
 use rust_decimal::Decimal;
 use std::time::UNIX_EPOCH;
 
-// use hex_literal::hex;
-use sha2::digest::typenum::private::IsGreaterPrivate;
+
 use sha2::{Digest, Sha256};
 
 trait CryptoHash {
@@ -14,6 +13,15 @@ trait CryptoHash {
     fn provide_bytes(&self) -> Vec<u8>;
 }
 
+struct Blockchain {
+    genesis_block: LinkedBlock,
+}
+
+impl Blockchain {
+    fn new() -> Blockchain {
+
+    }
+}
 
 struct LinkedBlock {
     prev: Option<Box<LinkedBlock>>,
@@ -25,20 +33,28 @@ struct Block {
     hash: Vec<u8>,
     txs: Vec<Tx>,
     nonce: u32,
-    prev_hash: Option<Vec<u8>>,
+    prev_hash: Vec<u8>,
 }
 
 impl Block {
-    fn mine(txs: Vec<Tx>, prev_hash: Option<Vec<u8>>) -> Block {
+    fn mine(txs: Vec<Tx>, prev_hash: Vec<u8>) -> Block {
         let timestamp = UNIX_EPOCH.elapsed().unwrap().as_millis();
+        let target_prefix = b"\x00\x00";
 
         let (nonce, hash) = (0..=u32::MAX)
             .find_map(|nonce| {
                 let mut hasher = Sha256::new();
-                hasher.update(b"hello world");
-                let result = hasher.finalize();
-                if result < result {
-                    Some((nonce, result.to_vec()))
+
+                hasher.update(b"Block:v1:");
+                for tx in &txs {
+                    hasher.update(tx.calculate_crypto_hash());
+                }
+                hasher.update(nonce.to_le_bytes());
+                hasher.update(prev_hash.as_slice());
+
+                let hash = hasher.finalize();
+                if hash.starts_with(target_prefix) {
+                    Some((nonce, hash.to_vec()))
                 } else {
                     None
                 }
@@ -51,24 +67,6 @@ impl Block {
         }
     }
 }
-
-impl CryptoHash for Block {
-    fn provide_bytes(&self) -> Vec<u8> {
-        let mut bytes = vec![];
-
-        bytes.append(b"UTXOReference:v1:".to_vec().as_mut());
-        bytes.append(self.timestamp.to_le_bytes().to_vec().as_mut());
-        bytes.push(':'.try_into().unwrap());
-        bytes.append(self.input.calculate_crypto_hash().as_mut());
-        bytes.push(':'.try_into().unwrap());
-        bytes.append(self.output.calculate_crypto_hash().as_mut());
-        bytes.push(':'.try_into().unwrap());
-        bytes.append(self.signature.clone().as_mut());
-
-        bytes
-    }
-}
-
 
 struct Tx {
     timestamp: u128,
