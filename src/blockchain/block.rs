33a91cbd69b21c046c137530fdca8f0c5e205923
Daniil Bastrich <bastrich+github@pm.me>
2025-08-20 23:18:42 +0200

    Add fee processing

--- src/blockchain/block.rs
+++ src/blockchain/block.rs
@@ -1,6 +1,7 @@
 use std::rc::Rc;
 use std::time::UNIX_EPOCH;
 use rust_decimal::Decimal;
+use rust_decimal::prelude::Zero;
 use sha2::{Digest, Sha256};
 use crate::blockchain::CryptoHash;
 use crate::blockchain::tx::Tx;
@@ -27,15 +28,29 @@ impl Block {
         let timestamp = UNIX_EPOCH.elapsed().unwrap().as_millis();
         let target_prefix = b"\x00\x00";
 
-        let reward_tx = Tx::new(
-            vec![],
-            vec![Rc::new(UTXOData {
-                amount: Decimal::from(50),
+        let mut coinbase_outputs = vec![Rc::new(UTXOData {
+            amount: Decimal::from(50),
+            pubkey: author_pubkey.clone(),
+        })];
+        let fee: Decimal  = txs.iter()
+            .map(|tx| {
+                let inputs_sum: Decimal = tx.inputs.iter().map(|i| i.data.upgrade().unwrap().amount).sum();
+                let outputs_sum: Decimal  = tx.outputs.iter().map(|o| o.amount).sum();
+                inputs_sum - outputs_sum
+            })
+            .sum();
+        if fee > Decimal::zero() {
+            coinbase_outputs.push(Rc::new(UTXOData {
+                amount: Decimal::from(fee),
                 pubkey: author_pubkey.clone(),
-            })],
+            }))
+        }
+        let coinbase_tx = Tx::new(
+            vec![],
+            coinbase_outputs,
             author_private_key
         );
-        txs.insert(0, reward_tx);
+        txs.insert(0, coinbase_tx);
 
         let (nonce, hash) = (0..=u32::MAX)
             .find_map(|nonce| {
